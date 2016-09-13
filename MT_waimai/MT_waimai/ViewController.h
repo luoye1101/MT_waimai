6d28e917d20b19ed9cac3f5c523bd024ae12b685
huangyueqi <huangyueqi1101@163.com>
2016-09-13 20:24:49 +0800

    创建项目

---
+++ MT_waimai/MT_waimai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MT_waimai
+//
+//  Created by 黄跃奇 on 16/9/13.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MT_waimai/MT_waimai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MT_waimai
+//
+//  Created by 黄跃奇 on 16/9/13.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
